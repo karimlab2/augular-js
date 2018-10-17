4f58b951091a9f147770b3e676f35d9cfda2da63
xpang4 <victoriqueee@gmail.com>
2018-10-16 20:08:45 -0500

    fixed minor bug

--- mod5_mol/src/public/signup/sign-up.controller.js
+++ mod5_mol/src/public/signup/sign-up.controller.js
@@ -11,10 +11,12 @@
         signup.menuitem_valid = true;
 
         signup.submit = function () {
+            console.log(signup.user.menuitem);
             var promise = signupService.validateMenuItem(signup.user.menuitem);
 
             promise.then( function (response) {
                 console.log("menuitem found");
+                signup.menuitem_valid = true;
                 signup.saved_user = signupService.saveUser(signup.user);
             })
             .catch(function (error) {
